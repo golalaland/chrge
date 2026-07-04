f455ee568766f73a0ca701a899f282e481fbcca4
golalaland <golalaland@proton.me>
2026-07-04 06:37:26 +0100

    Delete public directory

--- public/js/pages/print.js
+++
@@ -1,2 +0,0 @@
-import { renderComingSoon } from './coming-soon.js';
-renderComingSoon('print', 'Print Queue', 'Generate variable-data PDF sheets for batches, ready for thermal, roll, or A4 printing.');
